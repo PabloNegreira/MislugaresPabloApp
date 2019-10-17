61021e72a56719a2190dd90e5cfdc4e5e357402f
unknown <DAM223@acarballeira.local>
2019-10-17 09:32:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dam223/mislugarespabloapp/acercaDeActivity.java
@@ -0,0 +1,13 @@
+package com.example.dam223.mislugarespabloapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class acercaDeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+    }
+}
